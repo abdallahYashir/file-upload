7b26d1530734b63b6c9d5fdae1e667ff23956138
Abdallah <abdallah.ramsing@gmail.com>
2023-10-07 16:37:36 +0400

    Added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1a202c',
+        secondary: '#2d3748',
+        accent: '#e2e8f0',
+        highlight: '#edf2f7',
+        muted: '#718096',
+        success: '#48bb78',
+        info: '#4299e1',
+        warning: '#ecc94b',
+        danger: '#f56565',
+        light: '#f7fafc',
+        dark: '#2d3748',
+      },
+      fontFamily: {
+        'primary': 'Martian Mono, mono'
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
